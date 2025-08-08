270f8a252cb3866010d5d3a80914145768fff5a6
AP Pittman <ap.r.pittman@gmail.com>
2025-08-08 15:31:53 -0400

    updated plots, readme, ASA draft

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # abortion-amendments
 looking at citizen-initiated ballot measures to amend state constitutions to protect reproductive rights
+
+# workflow
+run `data_acquisition.R`
+then, run `analysis.R`
+then, run `abortionamendmentsdraft.qmd`
+
--- analysis.R
+++ analysis.R
@@ -1,95 +1,216 @@
-###### VERY QUICK initial analysis stuff
-
 library(tidyverse)
 library(here)
 library(did)
 
 load(here("data", "hps.Rdata"))
 
-### so, Ohio passed abortion protections in the 2023 election
-### AZ, CO, MO, MT, NV all passed them in the 2024 election
-# what if I compare Ohio to those
+wavedates <- hps |> 
+  select(t, startdate, enddate) |> 
+  unique()
+
 # treatment happens between t 12 and 13
 
-hps <- hps |> 
-  filter(state %in% c("OH", "AZ", "CO", "MO", "MT", "NV"))
 
-### Analysis 1: comparing women in Ohio to women in the other states
-
+############## Analysis 1: compare women in Ohio to women in *all* other states
 hps_1 <- hps |> 
   filter(sex == "f") |> 
   mutate(first_treat = case_match(state, 
                                   "OH" ~ 13,
-                                  .default = 0)) |> 
-  group_by(state, state_code, t, first_treat) |> 
-  summarize(phq4 = weighted.mean(phq4, w = pweight, na.rm = TRUE))
-
-
+                                  "MI" ~ NA_integer_, #michigan is already treated
+                                  .default = 0),
+         phq4 = phq4-4) |> 
+  drop_na(phq4)
+  
 ############### run DID
-set.seed(623)
 did1 <- att_gt(data = hps_1,
-       yname = "phq4", 
-       tname = "t", 
-       idname = "state_code", 
-       gname = "first_treat")
+                 yname = "phq4", 
+                 tname = "t",
+                 clustervars = "state_code",
+                 gname = "first_treat",
+                 panel = FALSE,
+                 pl = TRUE,
+                 cores = 4)
 
-################# compute average treatment effect
+did1
+
+did1_w <- att_gt(data = hps_1,
+               yname = "phq4", 
+               tname = "t",
+               clustervars = "state_code",
+               gname = "first_treat",
+               weightsname = "pweight",
+               panel = FALSE,
+               pl = TRUE,
+               cores = 4)
+
+did1_w
+
+### save this for later
+save(did1, file = here("data", "did1.Rdata"), compress = "bzip2")
+save(did1_w, file = here("data", "did1_w.Rdata"), compress = "bzip2")
+
+################# compute aggte
 set.seed(623)
-aggte(did1, type = "dynamic") |> 
-  ggdid()
+dynamicte1 <- aggte(did1, type = "dynamic")
+
+set.seed(623)
+dynamicte1_w <- aggte(did1_w, type = "dynamic")
 
 
-############## analysis 2: compare men in OH to men in other states
+dynamicte1_w |> ggdid()
+
+#save this for later too
+save(dynamicte1, file = here("data", "dynamicte1.Rdata"), compress = "bzip2")
+save(dynamicte1_w, file = here("data", "dynamicte1_w.Rdata"), compress = "bzip2")
+
+
+
+
+#################### Analysis 2: compare women in Ohio to women in not-yet-treated states
+
 hps_2 <- hps |> 
-  filter(sex == "m") |> 
+  filter(sex == "f") |> 
+  filter(state %in% c("OH", "AZ", "CO", "MT", "MO", "NV")) |> 
   mutate(first_treat = case_match(state, 
                                   "OH" ~ 13,
-                                  .default = 0)) |> 
-  group_by(state, state_code, t, first_treat) |> 
-  summarize(phq4 = weighted.mean(phq4, w = pweight, na.rm = TRUE))
-  
+                                  "MI" ~ NA_integer_, #michigan is already treated
+                                  .default = 0),
+         phq4 = phq4-4) |> 
+  drop_na(phq4)
+
 ############### run DID
-set.seed(623)
 did2 <- att_gt(data = hps_2,
                yname = "phq4", 
-               tname = "t", 
-               idname = "state_code", 
-               gname = "first_treat")
+               tname = "t",
+               clustervars = "state_code",
+               gname = "first_treat",
+               panel = FALSE,
+               pl = TRUE,
+               cores = 4)
 
-################# compute average treatment effect
+save(did2, file = here("data", "did2.Rdata"), compress = "bzip2")
+
+did2_w <- att_gt(data = hps_2,
+               yname = "phq4", 
+               tname = "t",
+               clustervars = "state_code",
+               gname = "first_treat",
+               weightsname = "pweight",
+               panel = FALSE,
+               pl = TRUE,
+               cores = 4)
+
+
+did2_w
+
+save(did2_w, file = here("data", "did2_w.Rdata"), compress = "bzip2")
+
+################ compute aggte
 set.seed(623)
-aggte(did2)
+dynamicte2 <- aggte(did2, type = "dynamic")
+
+save(dynamicte2, file = here("data", "dynamicte2.Rdata"), compress = "bzip2")
 
 
+set.seed(623)
+dynamicte2_w <- aggte(did2_w, type = "dynamic")
 
-###################### Analysis 3 (more descriptive): compare women in OH to men in OH
+dynamicte2_w |> ggdid()
 
+save(dynamicte2_w, file = here("data", "dynamicte2_w.Rdata"), compress = "bzip2")
+
+
+######################################### Analyses 3 + 4: Men
 hps_3 <- hps |> 
-  filter(state == "OH") |> 
-  group_by(sex, t, startdate) |> 
-  summarize(phq4 = weighted.mean(phq4, w = pweight, na.rm = T))
-
-
-hps_3 |> 
-  ggplot(aes(x = startdate, y = phq4, group = sex, color = sex)) +
-  geom_line() +
-  geom_vline(xintercept = mdy("11-05-2023"), linetype = "dashed") +
-  theme_minimal() +
-  labs(title = "Ohioans' Scores on the PHQ-4",
-       y = "PHQ-4 Score Weighted Average",
-       x = "Week")
-
-
-hps |> 
-  mutate(first_treat = case_match(state,
+  filter(sex == "m") |>
+  mutate(first_treat = case_match(state, 
                                   "OH" ~ 13,
-                                  .default = 0)) |> 
-  group_by(state, first_treat, startdate, sex) |> 
-  summarize(phq4 = weighted.mean(phq4, w = pweight, na.rm = T)) |> 
-  ggplot(aes(x = startdate, y = phq4, group = state, color = as.factor(first_treat))) +
-  geom_line() +
-  facet_wrap("sex") +
-  scale_color_manual(values = c("gray", "red"))
-  
+                                  "MI" ~ NA_integer_, #michigan is already treated
+                                  .default = 0),
+         phq4 = phq4-4) |> 
+  drop_na(phq4)
+
+hps_4 <- hps |> 
+  filter(sex == "m") |> 
+  filter(state %in% c("OH", "AZ", "CO", "MT", "MO", "NV")) |> 
+  mutate(first_treat = case_match(state, 
+                                  "OH" ~ 13,
+                                  "MI" ~ NA_integer_, #michigan is already treated
+                                  .default = 0),
+         phq4 = phq4-4) |> 
+  drop_na(phq4)
+
+did3_w <- att_gt(data = hps_3,
+                 yname = "phq4", 
+                 tname = "t",
+                 clustervars = "state_code",
+                 gname = "first_treat",
+                 weightsname = "pweight",
+                 panel = FALSE,
+                 pl = TRUE,
+                 cores = 4)
+
+did4_w <- att_gt(data = hps_4,
+                 yname = "phq4", 
+                 tname = "t",
+                 clustervars = "state_code",
+                 gname = "first_treat",
+                 weightsname = "pweight",
+                 panel = FALSE,
+                 pl = TRUE,
+                 cores = 4)
+
+dynamicte3_w <- aggte(did3_w, type = "dynamic")
+dynamicte4_w <- aggte(did4_w, type = "dynamic")
+
+save(dynamicte3_w, dynamicte4_w, file = here("data", "suppanalyses.Rdata"))
+
+
+
+# visualizations
+
+load(here("data", "did1_w.Rdata"))
+load(here("data", "did2_w.Rdata"))
+load(here("data", "dynamicte1_w.Rdata"))
+load(here("data", "dynamicte2_w.Rdata"))
+
+
+did1_w
+did2_w
+
+
+dynamicte1_w
+dynamicte2_w
+
+ggdid(dynamicte1_w) +
+  ggtitle("Model 1: ATT -.1937 (95% CI [-.2679, -.1196])")
+ggdid(dynamicte2_w) +
+  ggtitle("Model 2: ATT -.2598 (95% CI [-.4651, -.0545])")
+
+
+hps_1 |> 
+  group_by(state, startdate) |> 
+  summarize(phq4 = weighted.mean(phq4, w = pweight)) |> 
+  mutate(ohio = if_else(state == "OH", 1, 0)) |> 
+  ggplot(aes(x = startdate, y = phq4)) +
+  geom_line(aes(group = state, color = as.factor(ohio))) +
+  scale_color_manual(values = c("grey", "blue")) +
+  geom_vline(aes(xintercept = mdy("Nov 8 2023")), color = "red") +
+  theme(legend.position = "none") +
+  labs(x = "Date",
+       y = "PHQ-4 Score (Weighted Average)")
+
+hps_2 |> 
+  group_by(state, startdate) |> 
+  summarize(phq4 = weighted.mean(phq4, w = pweight)) |> 
+  mutate(ohio = if_else(state == "OH", 1, 0)) |> 
+  ggplot(aes(x = startdate, y = phq4)) +
+  geom_line(aes(group = state, color = as.factor(ohio))) +
+  scale_color_manual(values = c("grey", "blue")) +
+  geom_vline(aes(xintercept = mdy("Nov 8 2023")), color = "red") +
+  theme(legend.position = "none") +
+  labs(x = "Date",
+       y = "PHQ-4 Score (Weighted Average)")
+
 
 
